66bfe695e678d726858a9baab81cb8a3e050e260
lijiahao <598188200@qq.com>
2023-02-27 23:56:23 +0800

    Initial commit

---
+++ ms-service/user-service-api/src/main/java/com/dh/mall/App.java
@@ -0,0 +1,13 @@
+package com.dh.mall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
